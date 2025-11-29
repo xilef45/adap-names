aeacd0a44975cf924bdc197533ee4fdf3dd77144
Felix Lang <felix.l.lang@fau.de>
2025-11-29 14:57:49 +0100

    version 1

--- src/adap-b04/names/AbstractName.ts
+++ src/adap-b04/names/AbstractName.ts
@@ -1,44 +1,157 @@
 import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { InvalidStateException } from "../common/InvalidStateException";
+import { MethodFailedException } from "../common/MethodFailedException";
 
 export abstract class AbstractName implements Name {
 
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(delimiter: string = DEFAULT_DELIMITER) {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        IllegalArgumentException.assert(delimiter != null, "delimiter null is not allowed");
+        IllegalArgumentException.assert(delimiter != undefined , "delimiter undefined is not allowed");
+        IllegalArgumentException.assert(delimiter.length != 1, "delimiter must be a char");
+
+        // Actions
+        this.delimiter = delimiter;
+
+        //Postcondition
+        AbstractName.assertIsAbstractName(this);
     }
 
     public clone(): Name {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        AbstractName.assertIsAbstractName(this);
+
+        // Actions
+        const cloned = Object.create(this);
+
+        // Postcondition
+        AbstractName.assertIsAbstractName(cloned, "cloned not type of AbstractName");
+
+        return cloned;
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        AbstractName.assertIsAbstractName(this); 
+        IllegalArgumentException.assert(delimiter !== null, "delimiter null is not allowed");
+        IllegalArgumentException.assert(delimiter !== "undefined" , "delimiter undefined is not allowed");
+
+        //Actions
+        let returnStringArray: string[]= [];
+        for(let i=0;i < this.getNoComponents();i++){
+            let component = this.getComponent(i);
+            returnStringArray.push(component);
+        }
+        const returnValue = returnStringArray.join(delimiter);
+
+        //Postconditions
+        MethodFailedException.assert(returnValue !== "undefined", "resulting String can't be null" )
+        MethodFailedException.assert(returnValue !== null, "resulting String can't be null" )
+
+        return returnValue;
     }
 
     public toString(): string {
-        return this.asDataString();
+        //Precondition
+        AbstractName.assertIsAbstractName(this); 
+        
+        //Actions
+        const returnValue = this.asDataString();
+
+        //Postconditions
+        MethodFailedException.assert(returnValue !== "undefined", "resulting String can't be null" )
+        MethodFailedException.assert(returnValue !== null, "resulting String can't be null" )
+
+        return returnValue;
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        AbstractName.assertIsAbstractName(this); 
+
+        //Actions
+        let returnStringArray: string[]= [];
+        for(let i=0;i < this.getNoComponents();i++){
+            let component = this.getComponent(i)
+            .split(ESCAPE_CHARACTER).join(ESCAPE_CHARACTER + ESCAPE_CHARACTER);
+            if (this.delimiter !== "") {
+                       component = component.split(this.delimiter).join(ESCAPE_CHARACTER + this.delimiter);
+            }
+            returnStringArray.push(component);
+        }
+        const returnValue = returnStringArray.join(this.delimiter);
+
+        //Postconditions
+        MethodFailedException.assert(returnValue !== "undefined", "resulting String can't be null" );
+        MethodFailedException.assert(returnValue !== null, "resulting String can't be null" );
+
+        return returnValue;
     }
 
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        AbstractName.assertIsAbstractName(this); 
+        IllegalArgumentException.assert(AbstractName.isName(other), "other not Instance of Name");
+
+        //Actions
+        const returnValue = this.getHashCode() === other.getHashCode();
+
+        //Postconditions
+        MethodFailedException.assert(typeof returnValue === "boolean", "Method produced non boolean result" );
+
+        return returnValue;
     }
 
     public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        AbstractName.assertIsAbstractName(this); 
+
+        //Actions
+        let hash = 0;
+        const str = this.asDataString();
+        for (let i = 0; i < str.length; i++) {
+            const char = str.charCodeAt(i);
+            hash = Math.imul(31, hash) + char | 0;
+        }
+        const returnValue = hash;
+
+        //Postconditions
+        MethodFailedException.assert(typeof returnValue === "number", "Method produced non number result" );
+
+        return returnValue;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        AbstractName.assertIsAbstractName(this); 
+
+        //Actions
+        const returnValue = this.getNoComponents() === 0;
+
+        //Postconditions
+        MethodFailedException.assert(typeof returnValue === "boolean", "Method produced non boolean result" );
+
+        return returnValue;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        AbstractName.assertIsAbstractName(this); 
+
+        //Actions
+        const returnValue = this.delimiter;
+
+        //Postconditions
+        MethodFailedException.assert(typeof returnValue === "string", "Method produced non string result" );
+        MethodFailedException.assert(returnValue != null, "delimiter null is not allowed");
+        MethodFailedException.assert(returnValue != undefined , "delimiter undefined is not allowed");
+        MethodFailedException.assert(returnValue.length != 1, "delimiter must be a char");
+
+        return returnValue;
     }
 
     abstract getNoComponents(): number;
@@ -51,7 +164,71 @@ export abstract class AbstractName implements Name {
     abstract remove(i: number): void;
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        AbstractName.assertIsAbstractName(this);
+        IllegalArgumentException.assert(AbstractName.isName(other), "other not Instance of Name");
+
+        //Actions
+        for(let i = 0; i < other.getNoComponents(); i++) {
+            this.append(other.getComponent(i))
+        }
+
+        //Postconditions
+        AbstractName.assertIsAbstractName(this);
+    }
+
+
+    //Helper
+    protected static isAbstractName(input: any): input is AbstractName {
+        return AbstractName.isName(input) && "delimiter" in input &&
+            typeof input.delimiter === "string" && input.delimiter.length === 1;
+    }
+
+    protected static isName(input: any): input is Name {
+        return "isEmpty" in input && typeof input.isEmpty === "function" &&
+            "getNoComponents" in input && typeof input.getNoComponents === "function" &&
+            "getComponent" in input && typeof input.getComponent === "function" &&
+            "setComponent" in input && typeof input.setComponent === "function" &&
+            "insert" in input && typeof input.insert === "function" &&
+            "append" in input && typeof input.append === "function" &&
+            "remove" in input && typeof input.remove === "function" &&
+            "concat" in input && typeof input.concat === "function" &&
+            "clone" in input && typeof input.clone === "function" &&
+            "asString" in input && typeof input.asString === "function" &&
+            "asDataString" in input && typeof input.asDataString === "function" &&
+            "getDelimiterCharacter" in input && typeof input.getDelimiterCharacter === "function" &&
+            "isEqual" in input && typeof input.isEqual === "function" &&
+            "getHashCode" in input && typeof input.getHashCode === "function";
+    }
+
+    protected static assertIsAbstractName(input:any, failedMessage:string ="Missing Methods of AbstractName"): void {
+        MethodFailedException.assert(AbstractName.isAbstractName(input), failedMessage); 
+    }
+
+    protected static isMasked( inputString: string, delimiter:string, escape_char: string) : boolean {
+        // Preconditions
+        IllegalArgumentException.assert(typeof inputString === "string", "Input must be a string");
+        IllegalArgumentException.assert(typeof delimiter === "string" && delimiter.length === 1, "Delimiter must be a single character");
+
+        // Checks
+        let isMasked = true;
+
+        for (let i = 0; i < inputString.length; i++) {
+            const char = inputString[i];
+
+            if (char === delimiter || char === escape_char) {
+                // Check if the character is escaped
+                if (i === 0 || inputString[i - 1] !== escape_char) {
+                    isMasked = false;
+                    break;
+                }
+            }
+        }
+
+        // Postconditions
+        MethodFailedException.assert(typeof isMasked === "boolean", "isMasked did not return a boolean");
+
+        return isMasked;
     }
 
 }
--- src/adap-b04/names/StringArrayName.ts
+++ src/adap-b04/names/StringArrayName.ts
@@ -1,17 +1,30 @@
 import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 import { AbstractName } from "./AbstractName";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { InvalidStateException } from "../common/InvalidStateException";
+import { MethodFailedException } from "../common/MethodFailedException";
 
 export class StringArrayName extends AbstractName {
 
     protected components: string[] = [];
 
     constructor(source: string[], delimiter?: string) {
-        super();
+        //Precondition for source
+        IllegalArgumentException.assert(true); //source
+               
+        //delim by super
+        super(delimiter);
+
+        //Action
+        this.components = source;
+
+        //postcondtion
+        StringArrayName.assertIsStringArrayName(this, "this not type of StringArrayName");
         throw new Error("needs implementation or deletion");
     }
 
-    public clone(): Name {
+    /*public clone(): Name {
         throw new Error("needs implementation or deletion");
     }
 
@@ -37,33 +50,118 @@ export class StringArrayName extends AbstractName {
 
     public getDelimiterCharacter(): string {
         throw new Error("needs implementation or deletion");
-    }
+    }*/
 
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        //Precondition
+        StringArrayName.assertIsStringArrayName(this);   
+        
+        //Actions
+        const retValue = this.components.length;
+
+        //Postconditions
+        StringArrayName.assertIsStringArrayName(this);
+        MethodFailedException.assert(retValue >= 0 , "NoCompents needs to bei in N0")
+        
+        return retValue;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        StringArrayName.assertIsStringArrayName(this);
+        MethodFailedException.assert(i > 0 && i < this.components.length, "Index out of bounds");
+
+        //Actions
+        const retValue = this.components[i];
+
+        //Postconditions
+        MethodFailedException.assert(typeof retValue === "string", "getComponent did not return a string");
+
+        return retValue;
     }
 
     public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+        // Precondition
+        StringArrayName.assertIsStringArrayName(this);
+        IllegalArgumentException.assert(i >= 0 && i < this.components.length, "Index out of bounds");
+        IllegalArgumentException.assert(AbstractName.isMasked(c,this.getDelimiterCharacter(),ESCAPE_CHARACTER)), "c not masked correct");
+
+        // Actions
+        this.components[i] = c;
+
+        // Postconditions
+        StringArrayName.assertIsStringArrayName(this);
+        MethodFailedException.assert(this.components[i] === c, "setComponent failed to set the component correctly");
     }
 
     public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+        // Precondition
+        StringArrayName.assertIsStringArrayName(this);
+        MethodFailedException.assert(i >= 0 && i <= this.components.length, "Index out of bounds");
+        IllegalArgumentException.assert(AbstractName.isMasked(c,this.getDelimiterCharacter(),ESCAPE_CHARACTER), "c not masked correct");
+
+        // Actions
+        this.components.splice(i, 0, c);
+
+        // Postconditions
+        StringArrayName.assertIsStringArrayName(this);
+        MethodFailedException.assert(this.components[i] === c, "insert failed to insert the component correctly")
     }
 
     public append(c: string) {
-        throw new Error("needs implementation or deletion");
+        // Precondition
+        StringArrayName.assertIsStringArrayName(this);
+        IllegalArgumentException.assert(AbstractName.isMasked(c,this.getDelimiterCharacter(),ESCAPE_CHARACTER), "c not masked correct");
+
+        // Actions
+        this.components.push(c);
+
+        // Postconditions
+        StringArrayName.assertIsStringArrayName(this);
+        MethodFailedException.assert(this.components[this.components.length - 1] === c, "append failed to add the component correctly");
     }
 
     public remove(i: number) {
-        throw new Error("needs implementation or deletion");
+         // Precondition
+        StringArrayName.assertIsStringArrayName(this);
+        MethodFailedException.assert(i >= 0 && i < this.components.length, "Index out of bounds");
+
+        const oldLen = this.getNoComponents();
+        // Actions
+        const removed = this.components.splice(i, 1);
+
+        // Postconditions
+        MethodFailedException.assert(removed.length === 1, "remove failed to remove the component correctly");
+        MethodFailedException.assert(oldLen-1 == this.getNoComponents(),"remove failed to remove the component correctly");
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        // Precondition
+        StringArrayName.assertIsStringArrayName(this);
+        StringArrayName.assertIsStringArrayName(other);
+
+        // Actions
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            this.components.push(other.getComponent(i));
+        }
+
+        // Postconditions
+        StringArrayName.assertIsStringArrayName(this);
+        MethodFailedException.assert(this.getNoComponents() >= (other as StringArrayName).getNoComponents(), "concat failed to concatenate components correctly");
+    }
+
+    // helper
+
+    private static assertIsStringArrayName(input: any,failedMessage:string ="Missing Methods of StringArrayName"){
+        InvalidStateException.assert(StringArrayName.isStringArrayName(input), failedMessage );
+    }
+
+    private static isStringArrayName(input: any): input is StringArrayName{
+        var testing : boolean = super.isAbstractName(input) &&
+            "components" in input && input.components instanceof Array;
+        for(var component in input.components)
+        {
+            testing = testing && typeof input.components[component] === "string";
+        }
+        return testing;
     }
 }
